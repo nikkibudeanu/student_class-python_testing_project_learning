4d45dbc9fd4b90f453d4fc275bd8c707ba08642b
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-04-27 16:38:23 +0000

    use setup method

--- test_student.py
+++ test_student.py
@@ -1,25 +1,36 @@
 import unittest
 from student import Student
 
+
 class TestStudent(unittest.TestCase):
     
+    @classmethod
+    def setUpClass(cls):
+        print("set up class")
+
+    def setUp(self):
+        print("setup")
+        self.student = Student("John", "Doe")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tear down Class")
+
+    def tearDown(self):
+        print("tear down")
+
     def test_full_name(self):
-        student = Student('John', 'Doe')
-
-        self.assertEqual(student.full_name, 'John Doe')
-    
-    def test_email(self):
-        student = Student('John', 'Doe')
-        
-        self.assertEqual(student.email, "john.doe@email.com")
-
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
 
     def test_alert_santa(self):
-        student = Student('John', 'Doe')
-        student.alert_santa()
+        print("test_alert_santa")
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
 
-        self.assertTrue(student.naughty_list)
+    def test_email(self):
+        print("test_email")
+        self.assertEqual(self.student.email, "john.doe@email.com")
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
