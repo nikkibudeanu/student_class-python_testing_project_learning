555e1df5c9063a7a9b7d25ad600673431488e80b
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-04-27 17:07:26 +0000

    use mocking method to create course schedfule fucntion

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # student_class-python_testing_project_learning
+
+## mocking 
+* A method to get a student’s course schedule from a fictional external API
+
+* Imitates an API call so successful and failed responses can be tested
+
+
+* Import patch from unittest.mock and use it to create a mock object
+
--- student.py
+++ student.py
@@ -1,10 +1,12 @@
-
 from datetime import date, timedelta
+import requests
+
 
 class Student:
     """ A Student class as a basis for method testing """
+
     def __init__(self, first_name, last_name):
-        self._first_name  = first_name
+        self._first_name = first_name
         self._last_name = last_name
         self._start_date = date.today()
         self.end_date = date.today() + timedelta(days=365)
@@ -13,17 +15,22 @@ class Student:
     @property
     def full_name(self):
         return f"{self._first_name} {self._last_name}"
-    
+
     @property
     def email(self):
         return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
 
-
     def alert_santa(self):
         self.naughty_list = True
-    
-    def apply_extensions(self, days):
-        self.end_date += timedelta(days=days)
-        
 
- 
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
+
+    def course_schedule(self):
+        response = requests.get(
+            f"https://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong"
